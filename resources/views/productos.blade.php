6b5195657a916b9a1b05e89894222079cf702d6a
rgarcia07 <jrcg070495@gmail.com>
2025-10-21 10:30:55 -0600

    Carpeta y archivo CSS para productos

---
+++ resources/views/productos.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productos</title>
+
+    <link href="{{ asset('css/products.css') }}" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/productos',[ProductosController::class,'index'])->name('productos'); 
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
